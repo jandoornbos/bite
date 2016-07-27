f9fcfd7f61ed2e1a807d600421b2f89cf680e7a1
Jan Doornbos <jandoornbos@live.nl>
2016-07-27 20:55:31 +0200

    Improved the 'playing with your food' code

--- bite/viewcontrollers/product/ProductViewController.swift
+++ bite/viewcontrollers/product/ProductViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SWRevealViewController
 
-class ProductViewController: BiteViewController, UITableViewDataSource, UITableViewDelegate, ProductCellDelegate {
+class ProductViewController: BiteViewController, UITableViewDataSource, UITableViewDelegate, ProductCellDelegate, UIDynamicAnimatorDelegate {
 
     // MARK: Interface Builder Outlets
     @IBOutlet weak var tableView: UITableView!
@@ -17,12 +17,19 @@ class ProductViewController: BiteViewController, UITableViewDataSource, UITableV
     
     // MARK: Properties
     var productInBasket: Bool = false
+    var movingImageView: UIImageView?
+    
+    var animator: UIDynamicAnimator!
+    var attachmentBehaviour: UIAttachmentBehavior!
+    
+    var startPoint: CGPoint!
     
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideBasket(false)
+        self.addAnimator()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +37,11 @@ class ProductViewController: BiteViewController, UITableViewDataSource, UITableV
     }
     
     // MARK: - View
+    
+    func addAnimator() {
+        self.animator = UIDynamicAnimator(referenceView: self.view)
+        self.animator.delegate = self
+    }
     
     func hideBasket(animated: Bool) {
         if animated {
@@ -47,6 +59,20 @@ class ProductViewController: BiteViewController, UITableViewDataSource, UITableV
         }, completion: nil)
     }
     
+    func addAttachmentBehaviour(point: CGPoint, startPoint: CGPoint) {
+        self.startPoint = startPoint
+        guard let movingImageView = self.movingImageView else { return }
+        let offset = UIOffsetMake(point.x - movingImageView.bounds.size.width / 2.0, point.y - movingImageView.bounds.size.height / 2.0)
+        let anchor = self.movingImageView?.center
+        self.attachmentBehaviour = UIAttachmentBehavior(item: movingImageView, offsetFromCenter: offset, attachedToAnchor: anchor!)
+        self.animator.addBehavior(self.attachmentBehaviour)
+        self.attachmentBehaviour.anchorPoint = startPoint
+        self.attachmentBehaviour.action = {
+            let transform = self.movingImageView!.transform
+            self.movingImageView?.transform = CGAffineTransformScale(transform, 1.4, 1.4)
+        }
+    }
+    
     // MARK: - UITableView DataSource & UITableView Delegate
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -56,28 +82,32 @@ class ProductViewController: BiteViewController, UITableViewDataSource, UITableV
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(ProductCell.reuseIdentifier) as! ProductCell
         cell.delegate = self
-        cell.bringSubviewToFront(cell.productImageView)
         return cell
     }
     
     // MARK: - ProductCell Delegate
     
-    func productCellStartedAnimation(sender: ProductCell) {
+    func productCellPanGestureBegan(imageView: UIImageView, pointInView: CGPoint, startPoint: CGPoint, sender: ProductCell) {
         self.tableView.scrollEnabled = false
-        self.tableView.bringSubviewToFront(sender)
-        sender.layer.zPosition = 99
-    }
-    
-    func productCellStartDragging(sender: ProductCell) {
+        self.movingImageView = UIImageView(image: sender.productImageView.image)
+        self.movingImageView?.frame = sender.productImageView.frame
+        self.movingImageView?.contentMode = sender.productImageView.contentMode
+        self.movingImageView?.center = startPoint
+        self.view.addSubview(self.movingImageView!)
         self.showBasket()
+        self.addAttachmentBehaviour(pointInView, startPoint: startPoint)
+        UIView.animateWithDuration(0.2) {
+            self.movingImageView?.transform = CGAffineTransformMakeScale(1.4, 1.4)
+        }
     }
     
-    func getSuperview() -> UIView {
+    func productCellGetSuperview() -> UIView {
         return self.view
     }
-    
-    func productCellImageLocation(location: CGPoint, sender: ProductCell) {
-        if CGRectContainsPoint(self.basketHitPoint.frame, location) {
+
+    func productCellImageLocationChanged(point: CGPoint, sender: ProductCell) {
+        self.attachmentBehaviour.anchorPoint = point
+        if CGRectContainsPoint(self.basketHitPoint.frame, point) {
             self.productInBasket = true
             self.basketHitPoint.backgroundColor = UIColor.greenColor()
         } else {
@@ -86,15 +116,25 @@ class ProductViewController: BiteViewController, UITableViewDataSource, UITableV
         }
     }
     
-    func productCellInBasket() -> Bool {
-        return self.productInBasket
-    }
-    
-    func productCellEndedAnimation(sender: ProductCell) {
+    func productCellPanGestureEnded(sender: ProductCell) {
+        self.animator.removeAllBehaviors()
+        if !self.productInBasket {
+            let snap = UISnapBehavior(item: self.movingImageView!, snapToPoint: self.startPoint)
+            self.animator.addBehavior(snap)
+            NSTimer.scheduledTimerWithTimeInterval(0.8, target: self, selector: #selector(self.removeMovingImage), userInfo: nil, repeats: false)
+        } else {
+            self.removeMovingImage()
+        }
         self.basketHitPoint.backgroundColor = Color.yellow()
         self.hideBasket(true)
-        self.tableView.scrollEnabled = true
         self.productInBasket = false
+    }
+    
+    func removeMovingImage() {
+        self.movingImageView?.removeFromSuperview()
+        self.movingImageView = nil
+        self.animator.removeAllBehaviors()
+        self.tableView.scrollEnabled = true
     }
 
 }
--- bite/viewcontrollers/product/cells/ProductCell.swift
+++ bite/viewcontrollers/product/cells/ProductCell.swift
@@ -9,12 +9,10 @@
 import UIKit
 
 protocol ProductCellDelegate {
-    func productCellStartedAnimation(sender: ProductCell)
-    func getSuperview() -> UIView
-    func productCellImageLocation(location: CGPoint, sender: ProductCell)
-    func productCellInBasket() -> Bool
-    func productCellEndedAnimation(sender: ProductCell)
-    func productCellStartDragging(sender: ProductCell)
+    func productCellPanGestureBegan(imageView: UIImageView, pointInView: CGPoint, startPoint: CGPoint, sender: ProductCell)
+    func productCellGetSuperview() -> UIView
+    func productCellImageLocationChanged(point: CGPoint, sender: ProductCell)
+    func productCellPanGestureEnded(sender: ProductCell)
 }
 
 class ProductCell: UITableViewCell {
@@ -26,19 +24,12 @@ class ProductCell: UITableViewCell {
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var addButton: UIButton!
     
-    var delegate: ProductCellDelegate? {
-        didSet {
-            self.animator = UIDynamicAnimator(referenceView: self.delegate!.getSuperview())
-        }
-    }
+    var delegate: ProductCellDelegate?
     var timer: NSTimer?
     var isDragging: Bool = false
     var draggingAllowed: Bool = false
     var startCenter: CGPoint!
     var productCenter: CGPoint!
-    
-    var animator: UIDynamicAnimator!
-    var attachmentBehaviour: UIAttachmentBehavior!
     
     var panGesture: UIPanGestureRecognizer!
     
@@ -46,124 +37,37 @@ class ProductCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(self.longpress(_:)))
-        gesture.minimumPressDuration = 0.1
-        gesture.delegate = self
-        self.productImageView.addGestureRecognizer(gesture)
         self.panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.handlePan(_:)))
         self.panGesture.delegate = self
         self.productImageView.addGestureRecognizer(self.panGesture)
         self.productCenter = self.productImageView.center
-    }
-    
-    override func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
     
-    func beginShrinkAnimation() {
-        self.delegate?.productCellStartedAnimation(self)
-        UIView.animateWithDuration(1.5, animations: {
-            self.productImageView.transform = CGAffineTransformMakeScale(0.5, 0.5)
-        })
-    }
-    
-    func cancelShrinkAnimation() {
-        self.draggingAllowed = false
-        self.delegate?.productCellEndedAnimation(self)
-        UIView.animateWithDuration(0.3, delay: 0, options: [ .BeginFromCurrentState ], animations: { 
-            self.productImageView.transform = CGAffineTransformIdentity
-        }, completion: nil)
-    }
-    
-    func beginDragAndDrop() {
-        self.delegate?.productCellStartDragging(self)
-        UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.9, options: [ .BeginFromCurrentState ], animations: {
-            self.productImageView.transform = CGAffineTransformMakeScale(1.6, 1.6)
-        }, completion: { finished in
-            UIView.animateWithDuration(0.2, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.0, options: [ .BeginFromCurrentState ], animations: {
-                self.productImageView.transform = CGAffineTransformMakeScale(1.4, 1.4)
-                }, completion: { finished2 in
-                    self.addAttachmentBehaviour()
-                    self.draggingAllowed = true
-            })
-        })
-    }
-    
-    func longpress(sender: UILongPressGestureRecognizer) {
-        if sender.state == .Began {
-            self.timer = NSTimer.scheduledTimerWithTimeInterval(1.5, target: self, selector: #selector(self.beginDragAndDrop), userInfo: nil, repeats: false)
-            self.beginShrinkAnimation()
-            self.pointInView = sender.locationInView(sender.view)
-        }
-        if sender.state == .Ended {
-            self.timer?.invalidate()
-            self.timer = nil
-            self.cancelShrinkAnimation()
-        }
-        if sender.state == .Changed {
-            let location = sender.locationInView(self)
-            if !self.draggingAllowed {
-                if !CGRectContainsPoint(self.productImageView.frame, location) {
-                    self.timer?.invalidate()
-                    self.timer = nil
-                    self.cancelShrinkAnimation()
-                }
-            }
-        }
-    }
-    
     func handlePan(sender: UIPanGestureRecognizer) {
         if sender.state == .Began {
-            let buttonCenter = CGPointMake(self.productImageView.bounds.origin.x + self.productImageView.bounds.size.width/2,
-                                               self.productImageView.bounds.origin.y + self.productImageView.bounds.size.height/2);
-            let p = self.productImageView.convertPoint(buttonCenter, toView: self.delegate!.getSuperview())
-            self.startCenter = p
-            self.pointInView = sender.locationInView(sender.view)
-        }
-        if sender.state == .Changed {
-            if self.draggingAllowed {
-                let anchor = sender.locationInView(self.delegate!.getSuperview())
-                self.delegate?.productCellImageLocation(anchor, sender: self)
-                self.attachmentBehaviour.anchorPoint = anchor
+            if let view = sender.view as? UIImageView {
+                let pointInView = sender.locationInView(sender.view)
+                let startPoint = self.delegate!.productCellGetSuperview().convertPoint(self.productImageView.center, fromView: self)
+                self.delegate?.productCellPanGestureBegan(view, pointInView: pointInView, startPoint: startPoint, sender: self)
             }
-        }
-        if sender.state == .Ended {
-            self.draggingAllowed = false
-            self.animator.removeAllBehaviors()
-            if !self.delegate!.productCellInBasket() {
-                let snap = UISnapBehavior(item: sender.view!, snapToPoint: self.startCenter)
-                self.animator.addBehavior(snap)
-            } else {
-                self.animateBack()
-            }
-            self.delegate?.productCellEndedAnimation(self)
+        } else if sender.state == .Changed {
+            let anchor = sender.locationInView(self.delegate!.productCellGetSuperview())
+            self.delegate?.productCellImageLocationChanged(anchor, sender: self)
+            self.productImageView.alpha = 0
+        } else if sender.state == .Ended {
+            self.delegate?.productCellPanGestureEnded(self)
+            NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: #selector(self.putImageBack), userInfo: nil, repeats: false)
         }
     }
     
-    func addAttachmentBehaviour() {
-        let pointWithinView = self.pointInView
-        let offset = UIOffsetMake(pointWithinView.x - productImageView.bounds.size.width / 2.0, pointWithinView.y - productImageView.bounds.size.height / 2.0)
-        let anchor = self.panGesture.locationInView(self.delegate!.getSuperview())
-        self.attachmentBehaviour = UIAttachmentBehavior(item: productImageView, offsetFromCenter: offset, attachedToAnchor: anchor)
-        self.attachmentBehaviour.action = {
-            if self.draggingAllowed {
-                let transform = self.productImageView.transform
-                self.productImageView.transform = CGAffineTransformScale(transform, 1.4, 1.4)
-            }
-        }
-        self.animator.addBehavior(self.attachmentBehaviour)
-    }
-    
-    func animateBack() {
-        self.productImageView.alpha = 0
-        self.productImageView.center = self.productCenter
-        UIView.animateWithDuration(0.5, delay: 0.5, options: [], animations: { 
-            self.productImageView.alpha = 1
-        }, completion: nil)
+    func putImageBack() {
+        UIView.animateWithDuration(0.3, animations: {
+            self.productImageView.alpha = 1.0
+        })
     }
 
 }
